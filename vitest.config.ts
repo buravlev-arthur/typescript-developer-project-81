650cda24e4e918742057e339364eae5d59ab939f
Arthur Buravlev <buravlev-arthur@yandex.ru>
2024-01-28 21:23:56 +0300

    Шаг 3: изменен тип отчёта покрытия тестов на "lcov"

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul'
+      provider: 'istanbul',
+      reporter: 'lcov',
     }
   },
 })
